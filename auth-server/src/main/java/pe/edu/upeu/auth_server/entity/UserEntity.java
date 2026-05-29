774a4e9678f7de35faf132f3622f703c721b0070
arlysanchez <arly.sanchez@upeu.edu.pe>
2026-05-29 11:04:15 -0500

    añadiendo auth-server

---
+++ auth-server/src/main/java/pe/edu/upeu/auth_server/entity/UserEntity.java
@@ -0,0 +1,73 @@
+package pe.edu.upeu.auth_server.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user_info")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+
+    public UserEntity() {
+    }
+
+    public UserEntity(Long id, String password, String username) {
+        this.id = id;
+        this.password = password;
+        this.username = username;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public static class Builder {
+        private Long id;
+        private String username;
+        private String password;
+
+        public Builder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder username(String username) {
+            this.username = username;
+            return this;
+        }
+
+        public Builder password(String password) {
+            this.password = password;
+            return this;
+        }
+        public UserEntity build() {
+            return new UserEntity(id, username, password);
+        }
+    }
+    public static Builder builder(){
+        return new Builder();
+    }
+}
---
+++ auth-server/src/main/java/pe/edu/upeu/auth_server/services/AuthService.java
@@ -0,0 +1,5 @@
+package pe.edu.upeu.auth_server.services;
+
+public interface AuthService {
+
+}
